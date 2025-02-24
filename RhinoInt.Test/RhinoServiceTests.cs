ff13e1990223b248a674498bcb0199eff50ae672
marco.teran <marco.teran@earlens.com>
2025-02-24 13:07:13 -0800

    Updating with manifest.yml Rhino dependencies

---
+++ RhinoInt.Test/RhinoServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using RhinoInt;
+using Interfaces;
+
+namespace RhinoInt.Test
+{
+    [TestFixture]
+    public class RhinoServiceTests
+    {
+        [Test]
+        public void RunTestCommand_DoesNotThrow()
+        {
+            IRhinoService service = new RhinoService();
+            Assert.DoesNotThrow(() => service.RunTestCommand());
+        }
+    }
+}
