9f04c061c0bd6160cc1a1b0fa215f3b1e979c026
marco.teran <marco.teran@earlens.com>
2025-02-25 19:15:27 -0800

    Future Dev note in Commons/Params/Reprocess for the implementation of Reprocess logic

--- Commons/Params/Reprocess.cs
+++ Commons/Params/Reprocess.cs
@@ -1,5 +1,12 @@
 ﻿using Commons.Interfaces;
 
+/// ***NOTE - DO NOT DELETE***
+/// this module will need to override the batch patient ID intake process if MODE != ALL
+/// Not sure of the logic here, but there will be an alternaitive provided config file
+/// that has the full patient id list, including PASS/FAIL/SKIPPED markers. Reprocess
+/// uses that list and markers in lieu of the original config patient ID list. I do not
+/// if the orig list is needed or if it is ok for it to be parsed and not used. TBD
+
 namespace Commons.Params
 {
     public class Reprocess
