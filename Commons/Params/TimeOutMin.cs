2924e6d4a8586cad73530a135e58108fd5e9052b
marco.teran <marco.teran@earlens.com>
2025-02-26 10:47:48 -0800

    timeOut.min and logic notes - please review when implementing time out logic for batch

--- Commons/Params/TimeOutMin.cs
+++ Commons/Params/TimeOutMin.cs
@@ -1,5 +1,11 @@
 ﻿using Commons.Interfaces;
 
+
+/// ***FUTURE DEV NOTE*** do not delete this. timeout.min is a fail safe for frozen scripts. after 3-5 scripts that run
+/// the timeout manager will avg script completion time and use that time*50% as the time out variable. 
+/// If the first batch, the ones that are avg, do not complete before the config.timeout.min and the are skipped. batch
+/// needs to close so the user can review the script on a single file. 
+
 namespace Commons.Params
 {
     public class TimeOutMin
